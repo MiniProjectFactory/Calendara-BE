dde542a9bf4298668389b2179b23e9fdf5f7bf19
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-14 22:09:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Calendara-be
---
+++ src/main/java/com/dev/calendara/CalendaraApplication.java
@@ -0,0 +1,13 @@
+package com.dev.calendara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendaraApplication.class, args);
+    }
+
+}
