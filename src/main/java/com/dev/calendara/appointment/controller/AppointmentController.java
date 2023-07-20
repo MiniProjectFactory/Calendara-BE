d4cdb2fdb68e3a9a3516e34ad9b62110aa418a5a
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-21 02:53:41 +0900

    refactor : baseresponseDto 사용하도록 변경

--- src/main/java/com/dev/calendara/appointment/controller/AppointmentController.java
+++ src/main/java/com/dev/calendara/appointment/controller/AppointmentController.java
@@ -3,6 +3,7 @@ package com.dev.calendara.appointment.controller;
 import com.dev.calendara.appointment.controller.dto.AppointmentCreateRequest;
 import com.dev.calendara.appointment.service.AppointmentService;
 import com.dev.calendara.appointment.service.dto.AppointmentCreateResponse;
+import com.dev.calendara.common.response.dto.BaseResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ public class AppointmentController {
     private final AppointmentService appointmentService;
 
     @PostMapping("/appointment")
-    public AppointmentCreateResponse createAppointment(@RequestBody AppointmentCreateRequest appointmentCreateRequest) {
-        return appointmentService.createAppointment(appointmentCreateRequest);
+    public BaseResponseDto<AppointmentCreateResponse> createAppointment(@RequestBody AppointmentCreateRequest appointmentCreateRequest) {
+        return BaseResponseDto.ok(appointmentService.createAppointment(appointmentCreateRequest));
     }
 }
