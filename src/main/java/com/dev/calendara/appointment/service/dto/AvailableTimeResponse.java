d962cfce2ca5b7b7c75f01ba10c533e765a64011
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-22 01:50:50 +0900

    test : 미팅 신청 할 수 있도록 생성하는 컨트롤러 테스트 작성

--- src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeResponse.java
+++ src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeResponse.java
@@ -1,11 +1,14 @@
 package com.dev.calendara.appointment.service.dto;
 
 import com.dev.calendara.availabletimes.AvailableTime;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
 public record AvailableTimeResponse(
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableStartTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableEndTime
 ) {
     public static AvailableTimeResponse of(AvailableTime availableTime) {
--- src/test/java/com/dev/calendara/appointment/service/AppointmentServiceTest.java
+++ src/test/java/com/dev/calendara/appointment/service/AppointmentServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
 
+@ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 class AppointmentServiceTest {
