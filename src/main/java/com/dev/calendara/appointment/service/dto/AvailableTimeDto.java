85fc77844ce0c4d923812df33cf99af4550ee08a
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-28 01:33:38 +0900

    feat : 미팅 신청 양식에 대한 정보를 얻기 구현

--- src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeDto.java
+++ src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeDto.java
@@ -1,17 +1,13 @@
 package com.dev.calendara.appointment.service.dto;
 
-import com.dev.calendara.availabletimes.AvailableTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record AvailableTimeResponse(
+public record AvailableTimeDto(
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableStartTime,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableEndTime
 ) {
-    public static AvailableTimeResponse of(AvailableTime availableTime) {
-        return new AvailableTimeResponse(availableTime.getAvailableStartTime(), availableTime.getAvailableEndTime());
-    }
 }
