8f85fda7227eba3a7a1026c8b9a8e820914da3f6
choiyoungkwon12 <hpwh4457@naver.com>
2023-07-28 01:52:11 +0900

    test : 미팅 신청 정보 가져오는 테스트 작성

--- src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeDto.java
+++ src/main/java/com/dev/calendara/appointment/service/dto/AvailableTimeDto.java
@@ -5,9 +5,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record AvailableTimeDto(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableStartTime,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
         LocalDateTime availableEndTime
 ) {
 }
