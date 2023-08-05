bff903a856dc69c1d4f1010459cd40904270dcac
choiyoungkwon12 <hpwh4457@naver.com>
2023-08-06 04:57:53 +0900

    test : 호스트가 오픈한 미팅에 대해서 신청한 내역을 가져오는 테스트코드 작성

--- src/test/java/com/dev/calendara/apply/controller/ApplyControllerTest.java
+++ src/test/java/com/dev/calendara/apply/controller/ApplyControllerTest.java
@@ -1,8 +1,12 @@
 package com.dev.calendara.apply.controller;
 
 import com.dev.calendara.apply.controller.dto.ApplyCreateRequest;
+import com.dev.calendara.apply.controller.dto.ApplyListRequest;
+import com.dev.calendara.apply.domain.enumeration.ApplyStatus;
 import com.dev.calendara.apply.service.ApplyService;
 import com.dev.calendara.apply.service.dto.ApplyCreateServiceResponse;
+import com.dev.calendara.apply.service.dto.AppointmentApplyListResponse;
+import com.dev.calendara.member.dto.MemberResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,8 +20,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -57,6 +63,39 @@ class ApplyControllerTest {
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.message").value("SUCCESS"))
                 .andExpect(jsonPath("$.data.applyId").value(1))
+        ;
+    }
+
+    @Test
+    @DisplayName("호스트는 본인이 오픈한 미팅에 대해 신청한 미팅 신청 내역을 확인할 수 있다.")
+    void getApplyAppointment() throws Exception {
+        // Given
+        AppointmentApplyListResponse response = new AppointmentApplyListResponse(1L,
+                ApplyStatus.WAIT,
+                LocalDateTime.of(2023, 8, 6, 18, 30),
+                LocalDateTime.of(2023, 8, 6, 19, 0),
+                "test",
+                new MemberResponse("test", "test@test.com")
+        );
+        when(applyService.getAppointmentApplyList(new ApplyListRequest(1L, 1L))).thenReturn(List.of(response));
+
+        // When Then
+        mockMvc.perform(
+                        get("/api/v1/apply/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .param("memberId", "1")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andExpect(jsonPath("$.data[0].applyId").value(1))
+                .andExpect(jsonPath("$.data[0].applyStatus").value(ApplyStatus.WAIT.toString()))
+                .andExpect(jsonPath("$.data[0].applyStartTime").value("2023-08-06T18:30:00"))
+                .andExpect(jsonPath("$.data[0].applyEndTime").value("2023-08-06T19:00:00"))
+                .andExpect(jsonPath("$.data[0].title").value("test"))
+                .andExpect(jsonPath("$.data[0].member.name").value("test"))
+                .andExpect(jsonPath("$.data[0].member.email").value("test@test.com"))
         ;
     }
 }
